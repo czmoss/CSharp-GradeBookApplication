9e192d179d081c6c290b39211f0c8218fa9730b2
Charles Moss <8564397+czmoss@users.noreply.github.com>
2020-02-11 01:24:48 -0800

    Override CalculateStatistics in class RankedGradeBook
    Checking that there are enough students for "ranked" grading allows the
    GradeBookTests.CreateStatisticsOverridesTests.OverrideCalculateStatistics-
    Test to pass.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,6 +11,17 @@ namespace GradeBook.GradeBooks
             Type = Enums.GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
